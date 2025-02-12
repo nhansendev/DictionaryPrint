8ddde1830aa9fa883068220aa98f1c33c4e08998
Nathan Hansen <9289200+nhansendev@users.noreply.github.com>
2025-02-11 23:04:24 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name="dictionaryprint",
+    version="0.1.0",
+    description="Fancy printing of dictionaries in a human-readable format",
+    url="https://github.com/nhansendev/DictionaryPrint",
+    author="Nathan Hansen",
+    author_email="nato5342@hotmail.com",
+    license="BSD 3-clause",
+    packages=["dict_print"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.14",
+    ],
+)
